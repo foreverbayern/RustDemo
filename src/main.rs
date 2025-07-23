881be10356de2794edeceac1c11557a8ee414830
limao <872426466@qq.com>
2025-07-23 21:59:56 +0800

    reference

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("length is  {len}");
+}
+
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
